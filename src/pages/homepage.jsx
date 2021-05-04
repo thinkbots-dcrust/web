f7de77f4b2817fa598755c945229c5a82a998d65
Tushar <iamtushar324@gmail.com>
2021-05-04 20:05:02 +0530

    Remove Blinking of comming soon
    Signed-off-by: Tushar <iamtushar324@gmail.com>

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,13 +1,13 @@
 import MainLogo from "components/MainLogo";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 function Homepage() {
-  return (
-    <MainWrapper>
-      <MainLogo />
-      <Heading>Coming Soon</Heading>
-    </MainWrapper>
-  );
+	return (
+		<MainWrapper>
+			<MainLogo />
+			<Heading>Coming Soon</Heading>
+		</MainWrapper>
+	);
 }
 
 const Heading = styled.h1`
@@ -15,31 +15,12 @@ color:white;
 font-size:20px;
 text-align:center;
 font-family:arial;
-animation-name:coming;
-		animation-timing-function:ease-in-out;
-		animation-duration:3s;
-		animation-direction:reverse;
-		animation-fill-mode:forward;
-		animation-iteration-count:infinite;
-		transform-origin:60% 52%;
-		transition-duration:2s;
-
-@keyframes coming {
-  0%{
-    opacity:0.8;
-  }
-  50%{
-    opacity:0;
-  }
-  100%{
-    opacity:0.8;
-  }
 }
-`
+`;
 const MainWrapper = styled.div`
-background-color:black;
-height:100vh;
-width:100%;
-`
+	background-color: black;
+	height: 100vh;
+	width: 100%;
+`;
 
 export default Homepage;
