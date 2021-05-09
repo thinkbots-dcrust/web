81fa014b607c681c81d6c93a61bb22ad984fe618
harshphogat <55057156+harshphogat@users.noreply.github.com>
2021-05-09 14:34:25 +0530

    Update homepage.jsx

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,49 +1,21 @@
-import MainLogo from "components/MainLogo";
+
 import styled from 'styled-components';
-import About from "components/About";
-import Featured from "components/Featured";
+import About from "components/Homepage/About";
+import Featured from "components/Homepage/Featured";
+import Top from "components/Homepage/Top";
 
 function Homepage() {
   return (
     <MainWrapper>
-      <MainLogo />
-      <Heading>Meet the T of DCRUST</Heading>
+      <Top />
       <About />
       <Featured />
     </MainWrapper>
   );
 }
 
-const Heading = styled.p`
-color:white;
-font-size:20px;
-text-align:center;
-font-family:'Bangers', cursive;
-animation-name:coming;
-		animation-timing-function:ease-in-out;
-		animation-duration:3s;
-		animation-direction:reverse;
-		animation-fill-mode:forward;
-		animation-iteration-count:infinite;
-		transform-origin:60% 52%;
-		transition-duration:2s;
-
-@keyframes coming {
-  0%{
-    opacity:0.8;
-  }
-  50%{
-    opacity:0;
-  }
-  100%{
-    opacity:0.8;
-  }
-}
-`
-
 const MainWrapper = styled.div`
-background-color:black;
-height:500vh;
+background-color:#28231D;
 width:100%;
 `
 
