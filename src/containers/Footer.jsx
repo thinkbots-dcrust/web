2cd395c9e9b66b32d01a7ca3c433059dbc2c281a
harshphogat <55057156+harshphogat@users.noreply.github.com>
2021-05-09 14:33:50 +0530

    Create Footer.jsx

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer(){
+    return(
+        <Container>
+            <Contact>
+
+            </Contact>
+        </Container>
+    )
+}
+
+export default Footer;
+
+const Container = styled.div`
+`
+
+const Contact= styled.p`
+`
