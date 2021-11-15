0f2b07f88ee4870337955e2a75ce6ffa60582a53
Vinay Matta <vinaymatta63@gmail.com>
2021-11-15 16:34:10 +0530

    added homepage model

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,22 +1,42 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
 import styled from "styled-components";
 import { baseColor } from "styles/base";
+import Drone from "components/Drone";
+import Hologram from "components/Hologram";
 
 function Homepage() {
+  const [show, setShow] = useState(false);
+
   return (
     <>
-      <canvas />
-      <MainWrapper>
-        <Title>TH!NKBOTS</Title>
-        <Para>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          distinctio nobis quam vero ut ab veniam ipsum cumque provident,
-          quibusdam vitae, aspernatur iusto doloremque itaque, quia voluptatum
-          perspiciatis minima cupiditate. Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Ipsam provident, esse facilis, ipsa
-          quasi quibusdam, culpa architecto quisquam aliquam aliquid blanditiis
-          quis veritatis soluta tenetur rem! Corporis dicta ullam sapiente?
-        </Para>
-      </MainWrapper>
+      <Canvas
+        style={{ height: "92vh", width: "100%" }}
+        camera={{ fov: 45, near: 0.1, far: 1000, position: [7, 0, 7] }}
+      >
+        <Suspense fallback={null}>
+          <Drone setShow={setShow} />
+          <pointLight args={[0xffffff, 1, 100]} position={[5, 5, 5]} />
+          {show && <Hologram />}
+        </Suspense>
+      </Canvas>
+      {show && (
+        <MainWrapper>
+          <Plate>
+            <Title>TH!NKBOTS</Title>
+            <Para>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              distinctio nobis quam vero ut ab veniam ipsum cumque provident,
+              quibusdam vitae, aspernatur iusto doloremque itaque, quia
+              voluptatum perspiciatis minima cupiditate. Lorem ipsum, dolor sit
+              amet consectetur adipisicing elit. Ipsam provident, esse facilis,
+              ipsa quasi quibusdam, culpa architecto quisquam aliquam aliquid
+              blanditiis quis veritatis soluta tenetur rem! Corporis dicta ullam
+              sapiente?
+            </Para>
+          </Plate>
+        </MainWrapper>
+      )}
     </>
   );
 }
@@ -27,7 +47,8 @@ const MainWrapper = styled.section`
   left: 0;
   right: 0;
   width: 80%;
-  padding: 30vh 0;
+  top: 0;
+  padding: 25vh 0;
   position: absolute;
 `;
 const Para = styled.p`
@@ -46,4 +67,7 @@ const Title = styled.div`
   }
 `;
 
+const Plate = styled.div`
+  padding: 30px;
+`;
 export default Homepage;
