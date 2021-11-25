da745dfcd92aaae0c74384ef1e347b3eec7dccb9
Atul Saini <atulsaini9886@gmail.com>
2021-11-26 00:52:36 +0530

    Added Flipcard

---
+++ src/components/FlipCard/Flipcard.jsx
@@ -0,0 +1,41 @@
+import "./FlipCard.css";
+import { BsHeart, BsInstagram, BsLinkedin } from "react-icons/bs";
+
+function FlipCard() {
+  return (
+    <div className="flipcard-container">
+      <div className="frame">
+        <div className="card">
+          <div className="flip hvr-outline-in">
+            <div className="front">
+              <h1>Thinkbots</h1>
+              <p className="flip-me">Flip Me</p>
+              <div className="line line-1"></div>
+              <div className="line line-2"></div>
+              <a>
+                <BsHeart className="fa-heart" />
+              </a>
+            </div>
+
+            <div className="back">
+              <i className="fa fa-heart"></i>
+              <h3>Atul</h3>
+              <div className="line line-3"></div>
+              <h2>Web Developer</h2>
+              <div className="social">
+                <a>
+                  <BsInstagram />
+                </a>
+                <a>
+                  <BsLinkedin />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FlipCard;
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -3,27 +3,31 @@ import NavBar from "containers/NavBar";
 
 import styled from "styled-components";
 import { BsInstagram, BsLinkedin } from "react-icons/bs";
+import FlipCard from "components/FlipCard/Flipcard";
 // props to be received from the backend...... props include info regarding the person
 function ProfilePage() {
   return (
     <ProfileOuter>
       <ProfileContainer>
-        <FlipcardSection>
-          {/* need to add fliping card */}
-          {/* the flip card needs to have 40% of the space */}
-        </FlipcardSection>
-        {/*  */}
-        <InfoSection>
-          {/* need to add info here */}
-          <UpperInfoSection>
-            {/* 
+        <UpperProfile>
+          <FlipcardSection>
+            {/* need to add fliping card */}
+            {/* the flip card needs to have 40% of the space */}
+            <FlipCard />
+          </FlipcardSection>
+          {/*  */}
+          <InfoSection>
+            {/* need to add info here */}
+            <UpperInfoSection>
+              {/* 
                    -Name (h2)
                    -current rating
                    -edit icon(h4)
                 */}
-          </UpperInfoSection>
-          <MainInfoSection>
-            {/* 
+              <h2>Name Surame</h2>
+            </UpperInfoSection>
+            <MainInfoSection>
+              {/* 
                   -username
                   -country
                   -state
@@ -33,15 +37,24 @@ function ProfilePage() {
                   -year
                   -gender
                 */}
-          </MainInfoSection>
-          <LowerInfoSection>
-            {/* 
+              <h4>username</h4>
+              <h4>username</h4>
+              <h4>username</h4>
+              <h4>username</h4>
+              <h4>username</h4>
+              <h4>username</h4>
+              <h4>username</h4>
+              <h4>username</h4>
+            </MainInfoSection>
+            <LowerInfoSection>
+              {/* 
                    -social links
                       -insta
                       -linkedin
                 */}
-          </LowerInfoSection>
-        </InfoSection>
+            </LowerInfoSection>
+          </InfoSection>
+        </UpperProfile>
         {/*  */}
         <PastPerformancesSection>
           {/* need to add past performances here */}
@@ -49,16 +62,27 @@ function ProfilePage() {
         </PastPerformancesSection>
         {/*  */}
       </ProfileContainer>
-      <Footer />
     </ProfileOuter>
   );
 }
 
 export default ProfilePage;
 
-const ProfileOuter = styled.div``;
-const ProfileContainer = styled.div``;
+const ProfileOuter = styled.div`
+  height: 99vh;
+  margin: auto;
+`;
+const ProfileContainer = styled.div`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
 
+const UpperProfile = styled.div`
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+`;
 const FlipcardSection = styled.div``;
 const InfoSection = styled.div``;
 const PastPerformancesSection = styled.div``;
